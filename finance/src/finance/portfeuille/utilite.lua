70ede571075bb47293f9dc76cc849fcf0d6fa49f
Faraj Meir <meirfaraj@gmail.com>
2017-01-08 09:49:49 +0100

    working on fonction utilite

--- finance/src/finance/portfeuille/utilite.lua
+++ finance/src/finance/portfeuille/utilite.lua
@@ -11,6 +11,9 @@ FonctionUtilite = Tmv(FONCTION_UTILITE_ID,FONCTION_UTILITE_ID_HEADER_ID)
  
 
 function FonctionUtilite:widgetsInit()
+ self:add(0,"Richesse init:","W0")
+ self:add(0,"Fonction Util U(W):","U(W)")
+ self:add(1,"proba gain/perte p1,g1;p2,g2 :","gpv") 
 end
 
 function FonctionUtilite:performCalc()
